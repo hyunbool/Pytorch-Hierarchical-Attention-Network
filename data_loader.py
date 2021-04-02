fb0008cae5c219cf6bc105485f36ea52fd89bb05
Hyunjin Bae <gloria9705@naver.com>
2021-04-02 16:29:43 +0900

    data_loader.py for segment

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,4 @@
-from torch.utils.data.dataset import Dataset
+wfrom torch.utils.data.dataset import Dataset
 import csv
 from nltk.tokenize import sent_tokenize, word_tokenize
 import torch
@@ -47,24 +47,19 @@ class MyGensimModel():
         self.word_vec_dim = word_vec_dim
 
 class MyDataLoader():
-    def __init__(self,
-                 train_path,
-                 valid_path,
-                 dict_path = None,
-                 batch_size = 32,
-                 tokenizer_name="word_tokenizer",
-                 max_sent_len=100,
-                 max_word_len=100):
+    def __init__(self, train_path, valid_path, dict_path = None, batch_size = 32, tokenizer_name="word_tokenizer", max_seg_len=30, max_sent_len=100, max_word_len=100):
 
-        ##Load vector from Word2vec
-
+        # 이전 단계에서 만들어 두었던 word2vec에서 임베딩 벡터 로드
         model_path = os.path.join(dict_path, tools.WORD2VEC_NAME)
 
         ## Update gensim infomation
         self.model = MyGensimModel(model_path)
 
+        # MyTokenizer: word_embeder.py의 클래스
+        # 전체 문서 읽어 문장 단위로 자르고, 단어 별로 토크나이징 진행
         self.tokenizer = MyTokenizer(tokenizer_name)
 
+        self.max_seg_len =  max_seg_len
         self.max_sent_len = max_sent_len
         self.max_word_len = max_word_len
 
@@ -73,6 +68,7 @@ class MyDataLoader():
         self.dict_path = dict_path
         self.batch_size = batch_size
 
+        self.data_seg_len = 0
         self.data_sent_len = 0
         self.data_word_len = 0
 
@@ -87,35 +83,58 @@ class MyDataLoader():
 
     def get_dataset(self, docs, labels):
         docs_ids = []
+        docs_seg_len = []
         docs_sent_len = []
         docs_word_len = []
 
+
         for doc in docs:
-            temp_index = [[self.model.word2index.get(word) if self.model.word2index.get(word) else 0 for word in
-                           self.tokenizer.tokenize(sentences)] for sentences in sent_tokenize(doc)]
+            segments = [[sentences for sentences in sent_tokenize(seg)] for seg in doc.split("\n")][:-1]
 
-            for sentence in temp_index:
-                ##Even though there is no word after preprocess procedure, must put something like "[UNK]" to run machine
-                if len(sentence) == 0:
-                    sentence.extend([0])
+            segment_temp_index = list()
+            segment_sent = list()  # 현재 문서의 segment 별 문장 개수
+            segment_sent_word = list() # 현재 문서의 segment 별 문장의 단어 개수
 
-            temp_index = [sentences[:self.max_word_len] for sentences in temp_index][:self.max_sent_len]
+            for seg in segments:
+                temp_index = [[self.model.word2index.get(word) if self.model.word2index.get(word) else 0 for word in
+                               self.tokenizer.tokenize(sentences)] for sentences in seg]
 
-            sent_len = len(temp_index)
-            word_len = [len(sent) for sent in temp_index]
 
-            #Update maximum word, sent Length of Documents
-            if sent_len > self.data_sent_len:
-                self.data_sent_len = sent_len
-            for temp_len in word_len:
-                if temp_len > self.data_word_len:
-                    self.data_word_len = temp_len
+                for sentence in temp_index:
+                    ##Even though there is no word after preprocess procedure, must put something like "[UNK]" to run machine
+                    if len(sentence) == 0:
+                        sentence.extend([0])
 
-            docs_ids.append(temp_index)
-            docs_sent_len.append(sent_len)
-            docs_word_len.append(word_len)
+                temp_index = [sentences[:self.max_word_len] for sentences in temp_index][:self.max_sent_len]
 
-        return DocumentDataset(docs_ids, docs_sent_len, docs_word_len, labels, max_word_len=self.max_word_len, max_sent_len=self.max_sent_len)
+                sent_len = len(temp_index)
+                segment_sent.append(sent_len)
+                word_len =  [len(sent) for sent in temp_index]
+                segment_sent_word.append(word_len)
+
+                #Update maximum word, sent Length of Documents
+                if sent_len > self.data_sent_len:
+                    self.data_sent_len = sent_len
+                for temp_len in word_len:
+                    if temp_len > self.data_word_len:
+                        self.data_word_len = temp_len
+
+                segment_temp_index.append(temp_index)
+
+            seg_len = len(segments)
+            if seg_len > self.data_seg_len:
+                self.data_seg_len = seg_len
+
+            docs_ids.append(segment_temp_index)
+
+            docs_seg_len.append(seg_len)
+            docs_sent_len.append(segment_sent)
+            docs_word_len.append(segment_sent_word)
+
+        print(docs_ids)
+        print("\n")
+
+        return DocumentDataset(docs_ids, docs_seg_len, docs_sent_len, docs_word_len, labels, max_word_len=self.max_word_len, max_sent_len=self.max_sent_len, max_seg_len=self.max_seg_len)
 
     def get_train_valid(self):
         docs, labels, train_class = get_data(self.train_path)
@@ -125,8 +144,8 @@ class MyDataLoader():
         valid = self.get_dataset(docs, labels)
 
         if self.compac_max_length():
-            train.set_max_len(max_sent_len=self.data_sent_len, max_word_len=self.data_word_len)
-            valid.set_max_len(max_sent_len=self.data_sent_len, max_word_len=self.data_word_len)
+            train.set_max_len(max_seg_len=self.max_seg_len, max_sent_len=self.data_sent_len, max_word_len=self.data_word_len)
+            valid.set_max_len(max_seg_len=self.max_seg_len, max_sent_len=self.data_sent_len, max_word_len=self.data_word_len)
         
         ##Put DataLoader
         train = DataLoader(train, batch_size=self.batch_size, shuffle=True)
@@ -143,67 +162,103 @@ class MyDataLoader():
         if self.max_sent_len >= self.data_sent_len:
             self.max_sent_len = self.data_sent_len
             changed = True
+        if self.max_sent_len >= self.data_seg_len:
+            self.max_seg_len = self.data_seg_len
+            changed = True
         return changed
 
 
 class DocumentDataset(Dataset):
 
-    def __init__(self, ids, sent_len, word_len, labels, max_word_len=10, max_sent_len=10):
+    def __init__(self, ids, seg_len, sent_len, word_len, labels, max_word_len=10, max_sent_len=10, max_seg_len=30):
         super(DocumentDataset, self).__init__()
 
         self.ids = ids
+        self.seg_len = seg_len
         self.sent_len = sent_len
         self.word_len = word_len
         self.labels = labels
         self.len = len(labels)
         self.max_word_len=max_word_len
         self.max_sent_len=max_sent_len
+        self.max_seg_len=max_seg_len
 
     def __len__(self):
         return self.len
 
-    def set_max_len(self, max_word_len, max_sent_len):
+    def set_max_len(self, max_seg_len, max_word_len, max_sent_len):
+        self.max_seg_len = max_seg_len
         self.max_word_len = max_word_len
         self.max_sent_len = max_sent_len
         return True
 
     def __getitem__(self, index):
         temp_index = self.ids[index]
+        temp_seg_len = self.seg_len[index]
         temp_sent_len = self.sent_len[index]
         temp_word_len = self.word_len[index]
         temp_labels = self.labels[index]
 
-        for sent in temp_index:
-            if len(sent) < self.max_word_len:
-                extended_words = [0 for _ in range(self.max_word_len - len(sent))]
-                sent.extend(extended_words)
 
-        if len(temp_index) < self.max_sent_len:
-            extended_sentences = [[0 for _ in range(self.max_word_len)] for _ in
-                                  range(self.max_sent_len - len(temp_index))]
+        print(temp_word_len)
+        with open("temp_word_len_before.txt", "w") as f:
+            f.write(str(temp_word_len) + "\n")
+        """
+        temp_index 패딩
+        """
+        # 1) 각 문장 & 세그먼트 패딩
+        for seg in temp_index:
+            for sent in seg:
+                if len(sent) < self.max_word_len:
+                    extended_words = [0 for _ in range(self.max_word_len - len(sent))]
+                    sent.extend(extended_words)
+
+            if len(seg) < self.max_sent_len:
+                extended_sentences = [[0 for _ in range(self.max_word_len)] for _ in
+                                      range(self.max_sent_len - len(seg))]
+                seg.extend(extended_sentences)
+
+        # 2) 각 문서 패딩
+        if len(temp_index) < self.max_seg_len:
+            extended_sentences = [[0 for _ in range(self.max_sent_len)] for _ in
+                                  range(self.max_seg_len - len(temp_index))]
             temp_index.extend(extended_sentences)
 
-        temp_index = [sentences[:self.max_word_len] for sentences in temp_index][:self.max_sent_len]
+        """
+        length에 패딩
+        """
+        for sentence in temp_word_len:
+            if len(sentence) < self.max_sent_len:
+                extended_word_len = [0 for _ in range(self.max_sent_len - len(sentence))]
+                sentence.extend(extended_word_len)
 
-        if len(temp_word_len) < self.max_sent_len:
-            extended_word_len = [0 for _ in range(self.max_sent_len - len(temp_word_len))]
-            temp_word_len.extend(extended_word_len)
         temp_word_len = temp_word_len[:self.max_sent_len]
 
+        if len(temp_sent_len) < self.max_seg_len:
+            extended_sent_len = [0 for _ in range(self.max_seg_len - len(temp_sent_len))]
+            temp_sent_len.extend(extended_sent_len)
+
+        temp_sent_len = temp_sent_len[:self.max_seg_len]
+
+
+
+
+        with open("temp_word_len.txt", "w") as f:
+            f.write(str(temp_word_len) + "\n")
         temp_index = torch.tensor(temp_index)
+        temp_seg_len = torch.tensor(temp_seg_len)
         temp_sent_len = torch.tensor(temp_sent_len)
         temp_word_len = torch.tensor(temp_word_len)
         temp_labels = torch.tensor(temp_labels)
 
-        return temp_index, temp_sent_len, temp_word_len, temp_labels
-
+        return temp_index, temp_seg_len, temp_sent_len, temp_word_len, temp_labels
 
 
 if __name__ == '__main__':
-    train_path = 'data/train.csv'
-    valid_path = 'data/test.csv'
-    dict_path = 'word2vec/3/word2vec.model'
-    loader = MyDataLoader(train_path, valid_path, dict_path, max_word_len=1, max_sent_len=1)
+    train_path = 'short_concat_train.csv'
+    valid_path = 'short_concat_test.csv'
+    dict_path = 'word2vec/3/'
+    loader = MyDataLoader(train_path, valid_path, dict_path, max_word_len=256, max_sent_len=30)
     train, valid, size = loader.get_train_valid()
 
 
